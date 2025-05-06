3f55e32a37c9faa009a863bd762f9df554d23286
branesh2k <braneshp24@gmail.com>
2025-05-06 19:46:31 +0530

    added css

--- tara_web/src/components/Header.jsx
+++ tara_web/src/components/Header.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import './Header.css'; 
 
-
-function Header(){
-    return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
+function Header() {
+    return (
+        <nav className="navbar">
+            <div className="container">
                 <a className="navbar-brand" href="/">TARA</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" aria-label="Toggle navigation">
+                    ☰
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="navbar-links">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            <a className="nav-link" href="/">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/">About Us</a>
@@ -21,8 +21,7 @@ function Header(){
                 </div>
             </div>
         </nav>
-    )
+    );
 }
-
 
 export default Header;
